395631a2507051a030135eedf78606f354505b07
Alex Laurin <alex1231.laurin@gmail.com>
2025-09-02 10:48:07 -0400

    created a new working class based on the TicketMachine

---
+++ Book.java
@@ -0,0 +1,40 @@
+
+/**
+ * Write a description of class Book here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Book
+{
+    // instance variables - replace the example below with your own
+    private int price;
+    private String title;
+
+    /**
+     * Constructor for objects of class Book
+     */
+    public Book(int cost,String name)
+    {
+        // initialise instance variables
+        price = cost;
+        title = name;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public String getTitle()
+    {
+        // put your code here
+        return title;
+    }
+    
+    public int getPrice()
+    {
+        return price;
+    }
+}
